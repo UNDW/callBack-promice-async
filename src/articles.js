8eb713df3f176508976833ff2a58d2c205f3f964
Oleg Kraev <Lega.WellnesS@gmail.com>
2022-07-06 21:03:22 +0300

    callBack

---
+++ src/articles.js
@@ -0,0 +1,39 @@
+import * as serverApi from "./db.js";
+
+function all(onSuccess, onError) {
+  serverApi.all((response) => {
+    let info = JSON.parse(response);
+
+    if (info.code === 200) {
+      onSuccess(info.data);
+    } else {
+      onError(info.status);
+    }
+  });
+}
+
+function one(id, onSuccess, onError) {
+  serverApi.get(id, (response) => {
+    let info = JSON.parse(response);
+
+    if (info.code === 200) {
+      onSuccess(info.data);
+    } else {
+      onError(info.status);
+    }
+  });
+}
+
+function remove(id, onSuccess, onError) {
+  serverApi.remove(id, (response) => {
+    let info = JSON.parse(response);
+
+    if (info.code === 200) {
+      onSuccess(info.data);
+    } else {
+      onError(info.status);
+    }
+  });
+}
+
+export { all, one, remove };
